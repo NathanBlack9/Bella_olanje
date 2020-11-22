780432718aebd3acca734fed3399f7785b2915b6
Suren <suren.khachatryan1999@bk.ru>
2020-11-22 12:19:46 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass        = require('gulp-sass');
+const rename      = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+// Static server
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/scss/*.+(scss|sass)")
+            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .pipe(rename({
+                prefix: "",
+                suffix: ".min",
+            }))
+            .pipe(autoprefixer({
+                overrideBrowserslist:  ['last 2 versions'],
+                cascade: false
+            }))//префиксы где нужно
+            .pipe(cleanCSS({compatibility: 'ie8'}))//очищается css
+            .pipe(gulp.dest("src/css"))//вывод в .css
+            .pipe(browserSync.stream());//обновление стр
+});
+
+gulp.task('watch', function() {
+    gulp.watch("src/scss/*.+(scss|sass)", gulp.parallel('styles'), browserSync.reload);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+
