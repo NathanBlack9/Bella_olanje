22fbf31ecd44bcbbbcf97fc6af01b1e85c638999
Suren <suren.khachatryan1999@bk.ru>
2020-11-24 21:03:30 +0300

    added fixed header js code. Make mixin for burger menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+let header = document.querySelector("#header");
+let headerHeight = header.offsetHeight;
+let scrollPos = window.scrollY;
 
+checkScroll(scrollPos,headerHeight);
+
+window.addEventListener('scroll', () => {
+    headerHeight = header.offsetHeight;
+    scrollPos = this.scrollY;
+    checkScroll(scrollPos,headerHeight);
+});
+
+function checkScroll(scrollPos,headerHeight) {
+    if( scrollPos > headerHeight){
+        header.classList.add('fixed');
+    }else{
+        header.classList.remove('fixed');
+    }
+}
